c137dc9a18f8eb3bdcdd0f64f5edbf6bea949772
Bridget Staley <lynnbridgegirl@gmail.com>
2022-07-11 18:38:34 -0600

    Initial front routes and stuff

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -16,6 +16,16 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/info',
+      name: 'info',
+      component: () => import('../views/GameInfo.vue')
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('../views/Admin.vue')
     }
   ]
 })
