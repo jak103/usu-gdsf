55779fdad623615157970545d6cf5129938b771d
Jacob Christensen <jacob.h.christensen@gmail.com>
2022-07-11 20:29:29 -0600

    Update to Vuetify 3 / Vue 3 / Vite

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-const app = createApp(App)
+loadFonts()
 
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,35 @@
-import { fileURLToPath, URL } from 'url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
+
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {    // <-- this object is added
-    host: true,
-    port: 3000
-  },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    vuetify({
+      autoImport: true,
+    }),
+  ],
+  define: { 'process.env': {} },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
+  resolve: {
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ]
+  },
+  */
 })
