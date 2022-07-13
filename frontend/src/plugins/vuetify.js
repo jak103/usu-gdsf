98a8a5a67ee8831c00589984021379340622c9c3
Bridget Staley <lynnbridgegirl@gmail.com>
2022-07-12 21:28:35 -0600

    Added dockerfile and vuetify components and directives

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -5,6 +5,11 @@ import 'vuetify/styles'
 // Vuetify
 import { createVuetify } from 'vuetify'
 
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 export default createVuetify(
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+  components,
+  directives
 )
