ce106f45968792886d681513ed6817a6a474e330
Bridget Staley <lynnbridgegirl@gmail.com>
2022-07-13 23:32:32 -0600

    Setup basic frontend, added usu colors, broke up some components

--- frontend/src/components/NavMenu.vue
+++ frontend/src/components/NavMenu.vue
@@ -1,29 +1,39 @@
 <template>
-  <v-list>
-    <v-list-item
-      prepend-avatar="https://randomuser.me/api/portraits/women/2.jpg"
-      title="Sandra Adams"
-      subtitle="sandra_a88@gmailcom"
-    ></v-list-item>
-  </v-list>
+  <div>
+    <v-toolbar color="tangerine">
+      Game Dev Store
+      <v-btn icon :to="`/`">
+        <v-icon>
+          mdi-home
+        </v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-list nav>
+      <v-list-item to="/">Home</v-list-item>
+      <v-list-item to="/about">About</v-list-item>
+      <v-list-item v-if="appBarDisplay" :to="`/info/${defaultGame}`">Game</v-list-item>
+    </v-list>
 
-  <v-divider></v-divider>
-
-  <v-list density="compact" nav>
-    <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
-    <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
-    <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
-  </v-list>
+      <v-btn to="/admin" color="secondary">Login</v-btn>
+  </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
-// Components
-
 
 export default defineComponent({
   name: 'NavMenu',
+
+  data: () => ({
+    defaultGame: 1
+  }),
+
+  computed: {
+    appBarDisplay() {
+      return this.$router.currentRoute.value.path !== '/';
+    }
+  },
 
   components: {
 
--- frontend/src/views/GameInfo.vue
+++ frontend/src/views/GameInfo.vue
@@ -1,3 +1,36 @@
-<template> 
-<p>Game info page</p>
+<template>
+	<v-container>
+		<h2>{{ gameTitle }} {{ $route.params.id }}</h2>
+
+		<v-card>
+			{{ rating }}
+			<v-img width="200" :src="image"></v-img>
+			<router-link :to="{ name: 'info', params: { id: '2' }}">To another game</router-link>
+	<!-- Title -->
+	<!-- Ratings -->
+	<!-- Overview: Image -->
+	<!-- Download game -->
+		</v-card>
+	</v-container>
 </template>
+
+<script>
+export default {
+	name: 'GameInfo',
+
+	data: () => ({
+		gameTitle: 'Big Blue Game',
+		rating: 3,
+		image: 'https://st2.depositphotos.com/2927537/7025/i/950/depositphotos_70253417-stock-photo-funny-monkey-with-a-red.jpg'
+	}),
+
+	computed: {
+
+	},
+
+	methods: {
+
+	}
+};
+
+</script>
