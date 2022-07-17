8830801a958ee0ccb2245933da4ac66b527d7e2b
hagen-larsen-hl <hagenjay7@gmail.com>
2022-07-17 16:46:21 -0600

    added library handler

---
+++ backend/api/library.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/jak103/usu-gdsf/log"
+	"github.com/labstack/echo/v4"
+)
+
+func library(c echo.Context) error {
+	return c.JSON(http.StatusOK, "Library handler")
+}
+
+func init() {
+	log.Info("Running libary init")
+	registerRoute(route{method: http.MethodGet, path: "/library", handler: library})
+}
