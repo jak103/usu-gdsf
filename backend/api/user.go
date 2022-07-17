f8ffdb9b4e793a9dbc06c610954a93f2ff61386c
hagen-larsen-hl <hagenjay7@gmail.com>
2022-07-17 16:56:45 -0600

    added user downloads endpoint

--- backend/api/user.go
+++ backend/api/user.go
@@ -16,9 +16,15 @@ func register(c echo.Context) error {
 	return c.JSON(http.StatusOK, "User registration handler")
 }
 
+func downloads(c echo.Context) error {
+	// Return games that a user has downloaded/played
+	return c.JSON(http.StatusOK, "User downloads handler")
+}
+
 func init() {
 	log.Info("Running user init")
 
 	registerRoute(route{method: http.MethodGet, path: "/user", handler: user})
-	registerRoute(route{method: http.MethodGet, path: "/register", handler: register})
+	registerRoute(route{method: http.MethodGet, path: "/user/register", handler: register})
+	registerRoute(route{method: http.MethodGet, path: "user/downloads", handler: downloads})
 }
