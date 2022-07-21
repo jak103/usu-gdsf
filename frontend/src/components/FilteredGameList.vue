7a385d31c20c674af59f860e88df17ced26d23e6
Elise Francis <elisefrancis@Elises-MacBook-Air.local>
2022-07-20 21:39:05 -0600

    create initial vue component for filtered game list

---
+++ frontend/src/components/FilteredGameList.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    {{ something }}
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'FilteredGameList',
+  
+  data: () => ({
+    something: 'This is where we will implement a filtered game list... We will need to pass in props.'
+  }),
+  
+  methods: {
+    
+  }
+}
+</script>
