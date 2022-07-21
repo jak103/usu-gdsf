f481738c1eaf7d47442789ed0719f08af5190155
Grant Bruneel <grant.ray.bruneel@aggiemail.usu.edu>
2022-07-20 18:58:54 -0600

    feat: add links to some things on footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,16 +1,31 @@
 <template>
-    <v-footer color="teal" app>
-    Bottom navigation info and copyright stuff &copy; 2022 
+  <v-footer color="teal" app>
+    <v-container>
+      <v-row>
+        <v-col> About </v-col>
+      </v-row>
+      <v-row>
+        <v-col> <a href="https://usu.edu"> Utah State University</a> </v-col>
+      </v-row>
+      <v-row>
+        <v-col> <a href="https://www.usu.edu/cs/"> USU CS Department</a></v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          Created by Summer 2022 CS 5890 Intro to DevOps Course &copy; 2022
+        </v-col>
+      </v-row>
+    </v-container>
   </v-footer>
 </template>
 
 <script>
-  import { defineComponent } from 'vue';
+import { defineComponent } from 'vue';
 
-  export default defineComponent({
-    name: 'Footer',
-    components: {},
-    data: () => ({}),
-    computed: {},
-  })
+export default defineComponent({
+  name: 'Footer',
+  components: {},
+  data: () => ({}),
+  computed: {},
+})
 </script>
