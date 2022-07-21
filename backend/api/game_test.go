ddd8428012d569348102fb7de3980bc27477c2ac
Jacobl21 <64654496+Jacobl21@users.noreply.github.com>
2022-07-20 21:06:47 -0600

    Added note to AllGames test

--- backend/api/game_test.go
+++ backend/api/game_test.go
@@ -11,6 +11,7 @@ func TestGameRoute(t *testing.T) {
 	assert.True(t, AssertResponseCode(t, http.MethodGet, "/game", 200))
 }
 
+//this will need to be changed eventually. It is looking for response 500 now but when we get the database up it will be 200.
 func TestGetAllGames(t *testing.T) {
-	assert.True(t, AssertResponseCode(t, http.MethodGet, "/games", 500) )
+	assert.True(t, AssertResponseCode(t, http.MethodGet, "/games", 500))
 }
