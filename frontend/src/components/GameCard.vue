d2c161a7c39d7732179fb40fa8ab726570499f5e
Elise Francis <elisefrancis@Elises-MacBook-Air.local>
2022-07-22 10:51:50 -0600

    add flex to columns and showGameOverview prop

--- frontend/src/components/GameCard.vue
+++ frontend/src/components/GameCard.vue
@@ -3,17 +3,17 @@
     <v-card class="flex">
         <v-card-title>{{gameTitle}}</v-card-title>
         <v-row class="ma-2">
-            <v-col cols="6">
-                <v-img width="200" :src="image"></v-img>       
+            <v-col class="flex xs=12 md=6">
+                <v-img ref="gameImage" width="200" :src="image"></v-img>       
                 <v-card-actions class="center">
                     <span class="grey--text text--lighten-2 text-caption">
                         ({{ rating }})
                     </span>
-                    <v-rating readonly v-model="rating" half-increments color="amber"></v-rating>
+                    <v-rating readonly v-model="rating" half-increments color="amber" size="x-small"></v-rating>
                 </v-card-actions>     
             </v-col>
-            <v-col cols="6" class="flex">
-                <v-card-subtitle>{{gameOverview}}</v-card-subtitle>
+            <v-col class="flex xs=12 md=6">
+                <v-card-subtitle v-if=showGameOverview>{{gameOverview}}</v-card-subtitle>
                 <v-card-actions>
                     <v-btn>
                         Play    
@@ -32,7 +32,11 @@ export default {
   props: {
     gameTitle: String,
     gameOverview: String,
-    rating: Float32Array,
+    showGameOverview: {
+        type: Boolean,
+        default: false
+    },
+    rating: Number,
     image: String
   },
   
