2917ad1a11263777211046f160188e9b3acba369
Kris Price <kristoffer.k.price@gmail.com>
2022-07-27 15:37:38 -0600

    New game handler WIP

--- backend/api/game.go
+++ backend/api/game.go
@@ -1,11 +1,22 @@
 package api
 
 import (
+	"github.com/jak103/usu-gdsf/models"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/jak103/usu-gdsf/db"
 	"github.com/jak103/usu-gdsf/log"
 	"github.com/labstack/echo/v4"
+)
+
+const (
+	// TODO these are placeholder form names for adding a new game ('/game' POST)
+	// TODO match these strings with the view's form names for '/game' POST
+	NAME    = "Name"
+	AUTHOR  = "Author"
+	VERSION = "Version"
 )
 
 func game(c echo.Context) error {
@@ -13,14 +24,14 @@ func game(c echo.Context) error {
 }
 
 func getAllGames(c echo.Context) error {
-	db, err := db.NewDatabaseFromEnv()
+	_db, err := db.NewDatabaseFromEnv()
 
 	if err != nil {
 		log.WithError(err).Error("Unable to use database")
 		return err
 	}
 
-	if result, err := db.GetAllGames(); err != nil {
+	if result, err := _db.GetAllGames(); err != nil {
 		log.Error("An error occurred while getting game records: %v", err)
 		return err
 	} else {
@@ -29,7 +40,33 @@ func getAllGames(c echo.Context) error {
 }
 
 func newGameHandler(c echo.Context) error {
-	// TODO #5 We should probably actually create a game here
+	// create random uint64 for new game ID
+	var id = rand.Uint64()
+	for {
+		// check for already existing ID
+		exists, _ := db.IdExists(id)
+		if !exists {
+			break
+		} else {
+			id = rand.Uint64()
+		}
+	}
+
+	// create new game db model
+	newGame := models.Game{
+		Id:           id,
+		Name:         c.FormValue(NAME),
+		Author:       c.FormValue(AUTHOR),
+		CreationDate: time.Now().Format("MM/DD/YYYY"),
+		Version:      c.FormValue(VERSION),
+	}
+
+	// Add new game to database
+	_db, dbGetErr := db.NewDatabaseFromEnv()
+	err := _db.AddGame(newGame)
+	// TODO error handling
+
+	// TODO register new route with ID
 
 	return c.JSON(http.StatusOK, "New game handler")
 }
