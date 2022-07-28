4a48e4c0d817727d1a6a938c0d2252a5a818aca8
Grant Bruneel <grant.ray.bruneel@aggiemail.usu.edu>
2022-07-27 19:12:16 -0600

    change footer to buttons from anchors

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -2,18 +2,12 @@
   <v-footer color="teal" app>
     <v-container>
       <v-row>
-        <v-col> About </v-col>
-      </v-row>
-      <v-row>
-        <v-col> <a href="https://usu.edu"> Utah State University</a> </v-col>
-      </v-row>
-      <v-row>
-        <v-col> <a href="https://www.usu.edu/cs/"> USU CS Department</a></v-col>
-      </v-row>
-      <v-row>
+        <v-btn v-for="link of links" :key="link.link" color="teal"> {{ link.name }}</v-btn>
+        <v-row>
         <v-col>
           Created by Summer 2022 CS 5890 Intro to DevOps Course &copy; 2022
         </v-col>
+        </v-row>
       </v-row>
     </v-container>
   </v-footer>
@@ -25,7 +19,12 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'Footer',
   components: {},
-  data: () => ({}),
+  data: () => ({
+    links: [
+      { name: "Utah State University", link: "https://www.usu.edu" },
+      { name: "USU Computer Science Department", link: "https://www.usu.edu/cs/" }
+    ]
+  }),
   computed: {},
 })
 </script>
