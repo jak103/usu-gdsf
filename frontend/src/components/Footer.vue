8b056525bab253e962f144f529c8ded3ac95314b
Grant Bruneel <grant.ray.bruneel@aggiemail.usu.edu>
2022-07-27 19:35:46 -0600

    Center and beautify text

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,14 +1,12 @@
 <template>
   <v-footer color="teal" app>
     <v-container>
-      <v-row>
-        <v-btn v-for="link of links" :key="link.link" color="teal"> {{ link.name }}</v-btn>
-        <v-row>
-        <v-col>
+        <v-row justify="center">
+        <v-btn v-for="link of links" :href="link.link" color="teal"> {{ link.name }}</v-btn>
+        <v-col cols="12" class="text-center">
           Created by Summer 2022 CS 5890 Intro to DevOps Course &copy; 2022
         </v-col>
         </v-row>
-      </v-row>
     </v-container>
   </v-footer>
 </template>
