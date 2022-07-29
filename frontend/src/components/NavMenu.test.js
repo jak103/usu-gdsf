923f75b7705ef23cff921c44a1dac444fdee5c80
epeterson42 <a02075248@usu.edu>
2022-07-28 22:54:16 -0600

    Started switch to jest

---
+++ frontend/src/components/NavMenu.test.js
@@ -0,0 +1,12 @@
+import { mount } from '@vue/test-utils'
+import menu from './NavMenu.vue'
+
+test('renders Nav Menu', () => {
+//   const wrapper = mount(menu)
+
+//   const todo = wrapper.get('[data-test="defaultGame"]')
+
+//   expect(todo).toBe('Learn Vue.js 3')
+
+expect(1).toEqual(1);
+})
