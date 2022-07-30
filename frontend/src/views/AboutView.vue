82fff5150c1fd816aafefef431b1e6b6b1d75740
KeVulc <KeVulc>
2022-07-30 08:47:39 -0600

    added wording to the about page and added names data

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -5,7 +5,7 @@
         About our Project
       </v-card-title>
       <v-card-text>
-        This is how our project works and what it is doing.
+        {{ words }}
       </v-card-text>
     </v-card>
 
@@ -25,7 +25,9 @@ export default {
   name: 'AboutView',
 
   data: () => ({
-    moreWords: 'This will be a page of general text and information about our project'
+    words: 'This application was created as a project for the Intro to DevOps and Cloud Based Services course at USU. Our Mission is to deliver and showcase student projects created for the CS 5410 - Game Development course at USU. Our Project works by using Docker and Google Cloud to deploy the application. Firestore is used for our database.',
+    moreWords: 'The Game Development Store Front is a continuous application developed by students and maintained by professors. Many have contributed and many more will contribute to this project. Below is a list of all of the students who have helped make this project what it is today.',
+    listOfNames: [{firstName: 'firstName1',  lastName: 'lastName1'}, {firstName: 'firstName2',  lastName: 'lastName2'}]
   })
 }
 </script>
