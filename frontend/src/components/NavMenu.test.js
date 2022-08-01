f19f72937496219f8bc93ab535680fd58232e629
epeterson42 <a02075248@usu.edu>
2022-08-01 08:41:48 -0600

    Got the test passing

--- frontend/src/components/NavMenu.test.js
+++ frontend/src/components/NavMenu.test.js
@@ -1,12 +1,8 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 import menu from './NavMenu.vue'
 
 test('renders Nav Menu', () => {
-//   const wrapper = mount(menu)
-
-//   const todo = wrapper.get('[data-test="defaultGame"]')
-
-//   expect(todo).toBe('Learn Vue.js 3')
-
-expect(1).toEqual(1);
+  const wrapper = mount(menu)
+  const todo = wrapper.get('[data-test="listItem1"]')
+  expect(todo.text()).toBe("Game Dev Store")
 })
