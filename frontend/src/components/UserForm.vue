f627c36ea69c15dfca69fb7c0b0e8b9be1fff6f2
Hailey Dennis <a02242676@usu.edu>
2022-08-01 12:55:30 -0600

    WIP user editing

---
+++ frontend/src/components/UserForm.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-row justify="center">
+    <v-dialog
+      persistent
+    >
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">User Profile</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+                sm="4"
+                md="6"
+              >
+                <v-text-field
+                  label="First Name*"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="4"
+                md="6"
+              >
+                <v-text-field
+                  label="Last Name*"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="Email*"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="Password*"
+                  type="password"
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue-darken-1"
+            text
+            @update="$emit('update:showSelf', false)">
+            Close
+          </v-btn>
+          <v-btn
+            color="blue-darken-1"
+            text
+            @update="$emit('update:showSelf', false)">
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'UserForm',
+  
+  data: () => ({
+  
+  }),
+
+  
+  methods: {
+
+  }
+}
+</script>
--- frontend/src/views/UsersPage.vue
+++ frontend/src/views/UsersPage.vue
@@ -1,6 +1,15 @@
 <template>
+<div>
 	<h1 class="mt-10 ml-13">Site Users</h1>
-  <v-table class="mt-4 mb-4 pl-10 pr-10">
+
+	<v-progress-circular
+		v-if="loading"
+		style="position: fixed; top: 40%; left: 50%"
+		indeterminate
+		size="64"
+  ></v-progress-circular>
+
+  <v-table v-else class="mt-4 mb-4 pl-10 pr-10">
     <thead>
       <tr>
         <th class="text-left">
@@ -12,48 +21,66 @@
       </tr>
     </thead>
     <tbody>
-			<!-- TODO: Add onclick event to go to users page -->
+			<!-- TODO: Add onclick event -->
       <tr
         v-for="user in users"
         :key="user.email"
 				style="cursor: pointer"
+				@click="handleClick"
       >
         <td>{{ user.name }}</td>
         <td>{{ user.email }}</td>
       </tr>
     </tbody>
   </v-table>
+
+	<UserForm v-if="showForm"></UserForm>
+	</div>
 </template>
 
 <script>
+import UserForm from '../components/UserForm.vue'
 export default {
-	name: 'UsersPage',
-	
-	data () {
-		return {
-			// Get list of users from database
-			users: [
-				{name: 'Hailey Dennis', email: 'haileydennis@example.com'},
-				{name: 'Test Account', email: 'test@example.com'},
-								{name: 'Hailey Dennis', email: 'haileydennis@example.com'},
-				{name: 'Test Account', email: 'test@example.com'},
-								{name: 'Hailey Dennis', email: 'haileydennis@example.com'},
-				{name: 'Test Account', email: 'test@example.com'},
-								{name: 'Hailey Dennis', email: 'haileydennis@example.com'},
-				{name: 'Test Account', email: 'test@example.com'},
-								{name: 'Hailey Dennis', email: 'haileydennis@example.com'},
-				{name: 'Test Account', email: 'test@example.com'},
-								{name: 'Hailey Dennis', email: 'haileydennis@example.com'},
-				{name: 'Test Account', email: 'test@example.com'},
-								{name: 'Hailey Dennis', email: 'haileydennis@example.com'},
-				{name: 'Test Account', email: 'test@example.com'},
-								{name: 'Hailey Dennis', email: 'haileydennis@example.com'},
-				{name: 'Test Account', email: 'test@example.com'},
-								{name: 'Hailey Dennis', email: 'haileydennis@example.com'},
-				{name: 'Test Account', email: 'test@example.com'},
+    name: "UsersPage",
+    data() {
+        return {
+            // Get list of users from database
+            users: [
+                { name: "Hailey Dennis", email: "haileydennis@example.com" },
+                { name: "Test Account", email: "test@example.com" },
+                { name: "Hailey Dennis", email: "haileydennis@example.com" },
+                { name: "Test Account", email: "test@example.com" },
+                { name: "Hailey Dennis", email: "haileydennis@example.com" },
+                { name: "Test Account", email: "test@example.com" },
+                { name: "Hailey Dennis", email: "haileydennis@example.com" },
+                { name: "Test Account", email: "test@example.com" },
+                { name: "Hailey Dennis", email: "haileydennis@example.com" },
+                { name: "Test Account", email: "test@example.com" },
+                { name: "Hailey Dennis", email: "haileydennis@example.com" },
+                { name: "Test Account", email: "test@example.com" },
+                { name: "Hailey Dennis", email: "haileydennis@example.com" },
+                { name: "Test Account", email: "test@example.com" },
+                { name: "Hailey Dennis", email: "haileydennis@example.com" },
+                { name: "Test Account", email: "test@example.com" },
+                { name: "Hailey Dennis", email: "haileydennis@example.com" },
+                { name: "Test Account", email: "test@example.com" },
+            ],
+            // Set to false when users have loading, default should be true
+            loading: false,
+						showForm: false,
+						selectedUser: {
+							name: "",
+							email: ""
+						}
+        };
+    },
+    components: { UserForm },
 
-			]
+		methods: {
+			handleClick() {
+				console.log("clicked")
+				this.showForm = true
+			}
 		}
-	}
 }
 </script>
