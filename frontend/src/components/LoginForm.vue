ca38fb051532aaaeec32d0ce192be5ea15ae7a0f
brennypoo <brendonearl99@gmail.com>
2022-08-01 19:37:48 -0600

    unsubmittable form

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -1,28 +1,26 @@
 <template>
   <v-card>
-    <div color="primary">
-      <v-card-title>Login Form</v-card-title>
+    <div>
+      <v-card-title class="bg-primary">Login Form</v-card-title>
     </div>
 
     <v-card-text>
-      <v-form>
+      <v-form v-model="isValid">
         <v-text-field 
             label="username" 
             v-model="username" 
-            required 
-            :rules="[v => !!v || 'Username is required']">
+            required >
         </v-text-field>
         <v-text-field 
             label="password" 
             v-model="password" 
             type="password" 
-            required 
-            :rules="[v => !!v || 'Password is required']">
+            required >
         </v-text-field>
       </v-form>
     </v-card-text>
     <v-card-actions>
-      <v-btn color="teal" elevation="7"> Login </v-btn>
+      <v-btn class="bg-teal" elevation="7"> Login </v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -32,6 +30,10 @@ export default {
   props: {
     source: String,
   },
+  data: () => ({
+    username: null,
+    password: null,
+  }),
 };
 </script>
 
