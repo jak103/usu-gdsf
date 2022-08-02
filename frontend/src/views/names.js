ff234a3c24d2547ddfbd70074f763c041a479cea
KeVulc <KeVulc>
2022-08-01 20:54:20 -0600

    added names

--- frontend/src/views/names.js
+++ frontend/src/views/names.js
@@ -1,6 +1,11 @@
 export default {
   names: [
     "Kevin Vulcano",
-    "Camila Vulcano"
+    "Camila Vulcano",
+    "Hailey Dennis",
+    "Clark Smith",
+    "Jayden Bates",
+    "Matthew Larsen",
+    "Benjamin Winslett"
   ]
 }
