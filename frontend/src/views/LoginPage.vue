a17e0c4be0a901c492aaffbae6653786f1c5b07a
camilavulcano <a02265570@aggies.usu.edu>
2022-08-03 16:19:06 -0600

    basic css

--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -3,18 +3,20 @@
 		<!-- File uploads: FormData -->
 		<h1>This is where you login. Thanks for stopping by.</h1>
 	</div>
-	<v-container>
-		<v-card>
+	<v-container style="max-width: 600px; ">
+		<v-card style="background-color: #10243c;">
 			<v-card-title>
-				<h3>Login</h3>
+				<h3 style=" color: #FFFFFF;" >Login</h3>
 			</v-card-title>
 			<v-divider></v-divider>
 			<v-list-item>
-				<input type="text" name="username" v-model="input.username" placeholder="Username" />
-				<input type="password" name="password" v-model="input.password" placeholder="Password" />
+				<input type="text" name="username" v-model="input.username" placeholder="Username" style="background-color: #FFFFFF; padding: 16px;" />
 			</v-list-item>
 			<v-list-item>
-				<button type="button" onclick="login()" >Login</button>
+				<input type="password" name="password" v-model="input.password" placeholder="Password" style="background-color: #FFFFFF; padding: 16px" />
+			</v-list-item>
+			<v-list-item>
+				<button type="button" onclick="login()" style=" color: #FFFFFF; padding: 16px; background-color: #679ad6;" >Login</button>
 			</v-list-item>
 		</v-card>
 	</v-container>
