b21680a7dcb181f1d8a695e5da7b0243e0a430e4
eliseeef <efrancis@DESKTOP-A5JON6P.localdomain>
2022-08-04 22:09:22 -0600

    can run jest but test is failing

---
+++ frontend/test/components/test-component.test.js
@@ -0,0 +1,7 @@
+import { shallowMount } from '@vue/test-utils'
+import GameCard from '../src/components/GameCard'
+
+test('GameCard', () => {
+    // render the component
+    const wrapper = shallowMount(GameCard)
+})
