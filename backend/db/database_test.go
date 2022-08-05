40c57427aa5657af407e0ee3a4c57b63df3a5a3d
pravinpoudel <prvnpoudel4@gmail.com>
2022-08-05 16:48:41 -0600

    extra game object removed

--- backend/db/database_test.go
+++ backend/db/database_test.go
@@ -36,25 +36,7 @@ var (
 	}
 )
 
-var (
-	game2 = models.Game{
-		Name:         "game2",
-		Developer:    "tester",
-		CreationDate: time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC),
-		Version:      "0.0.0",
-		Tags:         []string{"tag100", "tag101"},
-	}
 
-	game3 = models.Game{
-		Name:         "game3",
-		Developer:    "tester",
-		CreationDate: time.Date(1900, 1, 2, 0, 0, 0, 0, time.UTC),
-		Version:      "0.0.1",
-		Tags:         []string{"tag101", "tag110"},
-		Downloads:    36,
-		DownloadLink: "dummy1.test",
-	}
-)
 
 func TestDatabase_GameID(t *testing.T) {
 	_db, _ := NewDatabaseFromEnv()
@@ -80,14 +62,14 @@ func TestDatabase_GameID(t *testing.T) {
 
 func TestDatabase_Tags(t *testing.T) {
 	_db, _ := NewDatabaseFromEnv()
-	id0, _ := _db.AddGame(game2)
-	id1, _ := _db.AddGame(game3)
+	id0, _ := _db.AddGame(game0)
+	id1, _ := _db.AddGame(game1)
 
-	game2.Id = id0
-	game3.Id = id1
+	game0.Id = id0
+	game1.Id = id1
 
-	res0, _ := _db.GetGamesByTags([]string{"tag100"}, false)
-	res1, _ := _db.GetGamesByTags([]string{"tag101"}, false)
+	res0, _ := _db.GetGamesByTags([]string{"tag000"}, false)
+	res1, _ := _db.GetGamesByTags([]string{"tag001"}, false)
 	res3, _ := _db.GetGamesByTags([]string{"bad tag"}, false)
 
 	// result size
@@ -96,11 +78,11 @@ func TestDatabase_Tags(t *testing.T) {
 	assert.Equal(t, 0, len(res3))
 
 	// result elements
-	assert.Contains(t, res0, game2)
-	assert.Contains(t, res1, game2)
-	assert.Contains(t, res1, game3)
+	assert.Contains(t, res0, game0)
+	assert.Contains(t, res1, game1)
+	assert.Contains(t, res1, game1)
 
 	// cleanup
-	_db.RemoveGame(game2)
-	_db.RemoveGame(game3)
+	_db.RemoveGame(game0)
+	_db.RemoveGame(game1)
 }
