33a7a01f72af4901ca32c5a85d00073af44e4e20
Abigail Allen <abigail.allen19@aggiemail.usu.edu>
2022-08-05 12:28:27 -0600

    added most of the contributors. Updated footer buttons. Both link to 'about' page. Working on external link

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,49 +1,56 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      app
-      color="primary"
-    >
-      <NavMenu/>
-    </v-navigation-drawer>
+<v-app>
+<v-navigation-drawer
+app
+color="primary"
+>
+<NavMenu/>
+</v-navigation-drawer>
 
-  <!-- Sizes your content based upon application components -->
-  <v-main>
+<!-- Sizes your content based upon application components -->
+<v-main>
 
-    <!-- Provides the application the proper gutter -->
-    <v-container fluid>
-      <router-view></router-view>
-    </v-container>
-  </v-main>
+<!-- Provides the application the proper gutter -->
+<v-container fluid>
+<router-view></router-view>
+</v-container>
+</v-main>
 
-  <v-footer color="primary" app
-  paddless
-  >
-  <v-row
-  justify="center"
-  no-gutters
-  >
-  <v-btn
-        v-for="link in links"
-        :key="link"
-        color="secondary"
-        text
-        rounded
-        class="my-2"
-        :href="link.url"
+<v-footer color="primary" app
+paddless
+>
+<v-row
+justify="center"
+no-gutters
+>
 
-      >
-        {{ link.text }}
+<v-btn
+  color="primary"
+  text
+  rounded
+  class="my-2"
+>
+      <router-link to="/about">USU CS</router-link>
       </v-btn>
-      <v-col
-        class="primary lighten-2 text-center black--text"
-        cols="12"
-      >
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-      </v-col>
-  </v-row>
-  </v-footer>
-  </v-app>
+
+<v-btn
+  color="white"
+  text
+  rounded
+  class="my-2"
+>
+      <router-link to="/about">About Us</router-link>
+      </v-btn>
+
+<v-col
+  class="primary lighten-2 text-center black--text"
+  cols="12"
+>
+  {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+</v-col>
+</v-row>
+</v-footer>
+</v-app>
 </template>
 
 <script>
@@ -51,30 +58,19 @@ import HomeView from './views/HomeView.vue'
 import NavMenu from './components/NavMenu.vue'
 
 export default {
-  name: 'App',
+name: 'App',
 
-  components: {
-    HomeView,
-    NavMenu
+components: {
+HomeView,
+NavMenu
 },
 
-  data: () => ({
-    links: [
-      {
-        text: "USU CS Department",
-        url: "www.usu.edu/cs"
-      },
-      {
-        text: "About Us",
-        url: "/about"
-      },
+data: () => ({
 
-    ],
+}),
 
-  }),
+computed: {
 
-  computed: {
-
-  }
+}
 }
 </script>
