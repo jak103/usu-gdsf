2d5d61623ee3bf0296c9b26cc4292d2e0fdc89dd
Abigail Allen <abigail.allen19@aggiemail.usu.edu>
2022-08-05 11:04:51 -0600

    updated

--- frontend/src/components/GameCard.vue
+++ frontend/src/components/GameCard.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <v-card
     class="mx-4 my-4"
     max-width="600px"
@@ -61,51 +60,4 @@
       show: false,
     }),
   }
-=======
-  <v-container>
-    <v-card class="flex">
-        <v-card-title>{{gameTitle}}</v-card-title>
-        <v-row class="ma-2">
-            <v-col class="flex xs=12 md=6">
-                <v-img ref="gameImage" width="200" :src="image"></v-img>       
-                <v-card-actions class="center">
-                    <span class="grey--text text--lighten-2 text-caption">
-                        ({{ rating }})
-                    </span>
-                    <v-rating readonly v-model="rating" half-increments color="amber" size="x-small"></v-rating>
-                </v-card-actions>     
-            </v-col>
-            <v-col class="flex xs=12 md=6">
-                <v-card-subtitle v-if=showGameOverview>{{gameOverview}}</v-card-subtitle>
-                <v-card-actions>
-                    <v-btn>
-                        Play    
-                    </v-btn>
-                </v-card-actions>
-            </v-col>
-        </v-row>
-	</v-card>
-  </v-container>
-</template>
-
-<script>
-export default {
-  name: 'GameCard',
-
-  props: {
-    gameTitle: String,
-    gameOverview: String,
-    showGameOverview: {
-        type: Boolean,
-        default: false
-    },
-    rating: Number,
-    image: String
-  },
-  
-  methods: {
-    
-  }
-}
->>>>>>> team2-main
 </script>
