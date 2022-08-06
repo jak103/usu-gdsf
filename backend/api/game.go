73ddc5b5017cf08321f0b63852b2f961877329aa
pravinpoudel <prvnpoudel4@gmail.com>
2022-08-06 15:23:17 -0600

    getGamesWithTags added Game model Struct binded with json

--- backend/api/game.go
+++ backend/api/game.go
@@ -3,7 +3,7 @@ package api
 import (
 	"net/http"
 	"time"
-
+	"strings"
 	"github.com/jak103/usu-gdsf/db"
 	"github.com/jak103/usu-gdsf/log"
 	"github.com/jak103/usu-gdsf/models"
@@ -31,8 +31,31 @@ func gameInfoHandler(c echo.Context) error {
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, "Database find game ID error")
 	}
-
 	return c.JSON(http.StatusOK, game)
+}
+
+func getGamesWithTags(c echo.Context) error {
+	// connect the db
+	_db, err := db.NewDatabaseFromEnv()
+	if err != nil {
+		log.WithError(err).Error("Database connection error in API getGamesWithTags")
+		return c.JSON(http.StatusInternalServerError, "Database connection error")
+	}
+	// // grab tags from context
+	// rawTags := c.FormValue(TAGS)
+
+	// // split string into array
+	// tags := strings.Split(rawTags, ",")
+
+	// // fetch games with tags
+	tags := c.QueryParam("tags")
+	tagsArray:= strings.Split(tags, "-")
+	games, err := _db.GetGamesByTags([]string{tagsArray[0], tagsArray[1]}, false)
+	if err != nil {
+		log.WithError(err).Error("Database GetGamesByTags error in API getGamesWithTags")
+		return c.JSON(http.StatusInternalServerError, "Database fetch games with tags error")
+	}
+	return c.JSON(http.StatusOK, games)
 }
 
 func getAllGames(c echo.Context) error {
@@ -82,4 +105,6 @@ func init() {
 	registerRoute(route{method: http.MethodGet, path: "/games", handler: getAllGames})
 	registerRoute(route{method: http.MethodPost, path: "/game", handler: newGameHandler})
 	registerRoute(route{method: http.MethodGet, path: "/info/:id", handler: gameInfoHandler})
+	registerRoute(route{method: http.MethodGet, path: "/game/tags", handler: getGamesWithTags})
+
 }
