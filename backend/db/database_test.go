5794223a3da2006deceb58ea4f32a177bc9b0263
Kris Price <kristoffer.k.price@gmail.com>
2022-08-05 18:48:07 -0600

    more robust tests

--- backend/db/database_test.go
+++ backend/db/database_test.go
@@ -9,6 +9,8 @@ import (
 )
 
 var (
+	_db, _ = NewDatabaseFromEnv()
+
 	game0 = models.Game{
 		Name:         "game0",
 		Rating:       3.5,
@@ -18,7 +20,7 @@ var (
 		Developer:    "tester",
 		CreationDate: time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC),
 		Version:      "0.0.0",
-		Tags:         []string{"tag000", "tag001"},
+		Tags:         []string{"tag0", "tag1"},
 		Downloads:    35,
 		DownloadLink: "dummy.test",
 	}
@@ -32,14 +34,13 @@ var (
 		Developer:    "tester",
 		CreationDate: time.Date(1900, 1, 2, 0, 0, 0, 0, time.UTC),
 		Version:      "0.0.1",
-		Tags:         []string{"tag001", "tag011"},
+		Tags:         []string{"tag1", "tag2"},
 	}
 )
 
-
-
 func TestDatabase_GameID(t *testing.T) {
-	_db, _ := NewDatabaseFromEnv()
+	// cleanup
+	t.Cleanup(cleanup)
 
 	// assign IDs on add
 	id0A, _ := _db.AddGame(game0)
@@ -50,26 +51,23 @@ func TestDatabase_GameID(t *testing.T) {
 
 	// assigned IDs
 	game0A, _ := _db.GetGameByID(id0A)
-	
 	game1A, _ := _db.GetGameByID(id1A)
 	assert.Equal(t, game0, game0A)
 	assert.Equal(t, game1, game1A)
-
-	// cleanup
-	_db.RemoveGame(game0)
-	_db.RemoveGame(game1)
 }
 
 func TestDatabase_Tags(t *testing.T) {
-	_db, _ := NewDatabaseFromEnv()
+	//cleanup
+	t.Cleanup(cleanup)
+
 	id0, _ := _db.AddGame(game0)
 	id1, _ := _db.AddGame(game1)
 
 	game0.Id = id0
 	game1.Id = id1
 
-	res0, _ := _db.GetGamesByTags([]string{"tag000"}, false)
-	res1, _ := _db.GetGamesByTags([]string{"tag001"}, false)
+	res0, _ := _db.GetGamesByTags([]string{"tag0"}, false)
+	res1, _ := _db.GetGamesByTags([]string{"tag1"}, false)
 	res3, _ := _db.GetGamesByTags([]string{"bad tag"}, false)
 
 	// result size
@@ -81,8 +79,9 @@ func TestDatabase_Tags(t *testing.T) {
 	assert.Contains(t, res0, game0)
 	assert.Contains(t, res1, game1)
 	assert.Contains(t, res1, game1)
+}
 
-	// cleanup
+func cleanup() {
 	_db.RemoveGame(game0)
 	_db.RemoveGame(game1)
 }
