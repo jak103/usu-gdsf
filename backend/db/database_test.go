04d0298d83114304627e6d10c52f73a0143eb426
pravinpoudel <prvnpoudel4@gmail.com>
2022-08-07 00:42:35 -0600

    SortGames test code without assert case added

--- backend/db/database_test.go
+++ backend/db/database_test.go
@@ -4,14 +4,14 @@ import (
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
-	// "fmt"
+	"fmt"
 	"github.com/jak103/usu-gdsf/models"
 )
 
 var (
 	_db, _ = NewDatabaseFromEnv()
 
-	game0 = models.Game{
+game0 = models.Game{
 		Name:         "game0",
 		Rating:       3.5,
 		TimesPlayed:  1,
@@ -91,6 +91,26 @@ func TestDatabase_Tags(t *testing.T) {
 	assert.Contains(t, res0, game0)
 	assert.Contains(t, res1, game1)
 	assert.Contains(t, res1, game1)
+}
+
+func TestSortGames(t *testing.T){
+	_db, _ := NewDatabaseFromEnv()
+	
+	//just to make it safe if there is incomplete test condition 
+	_db.RemoveGameByTag("tag0");
+	_db.RemoveGameByTag("tag1");
+	_db.RemoveGameByTag("tag2");
+	//cleanup
+	t.Cleanup(cleanup)
+
+	id0, _ := _db.AddGame(game0)
+	id1, _ := _db.AddGame(game1)
+
+	game0.Id = id0
+	game1.Id = id1
+
+	res, _ := _db.SortGames("_id", 1);
+	fmt.Printf("%+v", res)
 }
 
 func cleanup() {
