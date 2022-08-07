efc9d28451c7fd78258146356278fc1a9d401724
WinniCdM <winslett.benjamin@gmail.com>
2022-08-07 14:02:47 -0600

    Initial implementation of Game Info page. changed route to game info for better ui readability when loading data.

--- frontend/src/views/GameInfo.vue
+++ frontend/src/views/GameInfo.vue
@@ -1,36 +1,95 @@
 <template>
-	<v-container>
-		<h2>{{ gameTitle }} {{ $route.params.id }}</h2>
-
-		<v-card>
-			{{ rating }}
-			<v-img width="200" :src="image"></v-img>
-			<router-link :to="{ name: 'info', params: { id: '2' }}">To another game</router-link>
-	<!-- Title -->
-	<!-- Ratings -->
-	<!-- Overview: Image -->
-	<!-- Download game -->
-		</v-card>
+	<v-container data-test="game-data" v-if="!dataLoading && gameData !== null">
+		<h1 data-test="title" class="ml-10 pb-5">{{ gameData.Name }}</h1>
+		<v-row height=90vh>
+			<v-col data-test="image-col" cols="2">
+				<v-row
+					class="fill-height"
+					align-content="center"
+					justify="center"
+				>
+					<div v-if="gameData.ImagePath !== null || gameData.ImagePath !== ''">
+						<v-img					
+							width=100%
+							max-height="500"
+							:src="gameData.ImagePath"
+						></v-img>
+					</div>
+					<div v-else>
+						<v-tooltip>
+							<template v-slot:activator="{ props }">
+								<v-icon x-large color="gray" v-bind="props">
+									mdi-image-remove
+								</v-icon>
+							</template>
+							<span>No image(s) available</span>
+						</v-tooltip>	
+					</div>
+				</v-row>
+			</v-col>
+			<v-col data-test="data-col" cols="10">
+				<v-row>
+					<Rating
+						:rating="gameData.Rating"
+						:isReadOnly=true />
+				</v-row>
+				<v-row>
+					<div>Description: {{ gameData.Description }}</div>
+				</v-row>
+			</v-col>
+		</v-row>
+		<v-row>
+			<h3 data-test="title" class="ml-10 pb-5 pt-10">More Games Like {{ gameData.Name }}...</h3>
+		</v-row>
 	</v-container>
+	<Loading data-test="loadbar" v-if="dataLoading" :text="$route.params.name" containerStyle="height: 75vh"/>
+	<NoData v-if="!dataLoading && gameData === null" :text="$route.params.name" containerStyle="height: 75vh"></NoData>
+	<Footer />
 </template>
 
 <script>
-export default {
-	name: 'GameInfo',
+	import Rating from '../components/Rating.vue';
+	import Loading from '../components/Loading.vue';
+	import Footer from '../components/Footer.vue';
+	import axios from "axios";
+import NoData from '../components/NoData.vue';
+	export default {
+		name: 'GameInfo',
 
-	data: () => ({
-		gameTitle: 'Big Blue Game',
-		rating: 3,
-		image: 'https://st2.depositphotos.com/2927537/7025/i/950/depositphotos_70253417-stock-photo-funny-monkey-with-a-red.jpg'
-	}),
+		data: () => ({
+			dataLoading: true,
+			gameData: null
+		}),
+		components:{
+    Rating,
+    Loading,
+    Footer,
+    NoData
+},
+		computed: {
 
-	computed: {
+		},
 
-	},
-
-	methods: {
-
-	}
-};
-
+		methods: {
+			async getGameInfo(id) {
+				this.dataLoading = true;
+				await axios.get(`http://127.0.0.1:8080/info/${id}`)
+					.then(response => {
+						this.gameData = response.data;
+						this.gameData.ImagePath = "https://starwarsblog.starwars.com/wp-content/uploads/2017/05/1-star-wars-poster.jpg"
+						this.dataLoading = false
+					}).catch(error => {
+						console.log(error.response.data);
+						this.dataLoading = false
+						this.gameData = null
+					});
+			},
+			downloadGame(id){
+				// Download this game with id
+			}
+		},
+		created() {
+			this.getGameInfo(this.$route.params.id);
+		}
+	};
 </script>
