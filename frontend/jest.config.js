3ace8d934bb0eae84e31e6758e65159b10de63fd
Brandon Stone <bstone.cs@gmail.com>
2022-08-08 14:02:54 -0600

    Added scripting that forces Jest to target the correct test file.

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -4,24 +4,36 @@ module.exports = {
     "json",
     "vue",
  ],
+
   transform: {
       "^.+\\.js$": "babel-jest",
       ".*\\.(vue)$": "@vue/vue3-jest"
   },
+
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
+
   modulePaths: [
     '<rootDir>/src/',
     '<rootDir>/node_modules'
   ],
+
   moduleDirectories: [
     "src",
     "node_modules"
   ],
- moduleNameMapper: {
-    "@/(.*)": "<rootDir>/src/$1",
-  },
+
+  moduleNameMapper: {
+     "@/(.*)": "<rootDir>/test",
+   },
+
   testEnvironment: 'jsdom',
+
   testEnvironmentOptions: {
     customExportConditions: ['node', 'node-addons']
-  }
+  },
+
+  preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
+  testMatch: [
+    "**/test/**/*.spec.[jt]s?(x)",
+  ]
 }
