37ed7c81f18664c1ef3157076e70f6c56422c7fc
Kailey Bales <93214490+llamabyte@users.noreply.github.com>
2022-08-08 10:40:13 -0600

    Created simple tests for AddGames

---
+++ frontend/src/test/AddGame.spec.js
@@ -0,0 +1,30 @@
+import {mount} from '@vue/test-utils'
+import AddGame from '@/views/AddGame.vue'
+
+describe('AddGame.vue', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(AddGame);
+  });
+
+  test('renders a form', () => {
+    //check if the form is rendered
+    expect(wrapper.find('v-card').exists()).toBe(true)
+  }),
+  test('renders a form with inputs', () => {
+    //check if the form is rendered
+    expect(wrapper.find('input').exists()).toBe(true)
+  }),
+  test('renders a form with submit button', () => {
+    //check if the form is rendered
+    expect(wrapper.find('v-btn').exists()).toBe(true)
+  }),
+  test('sets the values', async () => {
+    const input = wrapper.find('input')
+  
+    await input.setValue('test name')
+
+  
+    expect(input.element.value).toBe('test name')
+  })
+})
