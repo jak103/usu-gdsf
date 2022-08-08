43775845854e0c1a428caeb7b157e695ce5d770a
Wesley Perrett <wesley.g.perrett@gmail.com>
2022-08-07 18:31:01 -0600

    added seeder data along with images to be used for testing

--- backend/db/dbSeed.go
+++ backend/db/dbSeed.go
@@ -32,5 +32,56 @@ const JSON_SEED_DATA = `[
     "Version": "1.x",
     "Tags": ["Sandbox", "Adventure"],
     "downloads": 160000000
-  }
+  },
+  {
+    "Id": "2",
+    "Name": "Call of Duty Warzone",
+    "Rating": 2.0,
+    "TimesPlayed": 1200,
+    "ImagePath": "../../frontend/src/assets/warzone.png",
+    "Description": "Exciting First Person Shooter",
+    "Developer": "Activision",
+    "CreationDate": "Friday April 11, 2000",
+    "Version": "1.x",
+    "Tags": ["Spring2020", "FPS"],
+    "downloads": 1000000
+  },
+  {
+    "Id": "3",
+    "Name": "Rust",
+    "Rating": 3.5,
+    "TimesPlayed": 40000000,
+    "ImagePath": "../../frontend/src/assets/rust.png",
+    "Description": "Survive among the rest",
+    "Developer": "Facepunch Studios Ltd",
+    "CreationDate": "Wednesday Dec 11, 2013",
+    "Version": "1.x",
+    "Tags": ["Survival", "Adventure", "Spring2018"],
+    "downloads": 1000000000
+  },
+  {
+    "Id": "4",
+    "Name": "Among Us",
+    "Rating": 5.0,
+    "TimesPlayed": 7000000000,
+    "ImagePath": "../../frontend/src/assets/minecraft.png",
+    "Description": "Work with your team to complete tasks. However, pay attention because your being hunted.",
+    "Developer": "InnerSloth LLC",
+    "CreationDate": "Tuesday May 1, 2018",
+    "Version": "1.x",
+    "Tags": ["Teamwork"],
+    "downloads": 1060989000
+  },
+  {
+    "Id": "5",
+    "Name": "Fall guys",
+    "Rating": 4.8,
+    "TimesPlayed": 36000000000,
+    "ImagePath": "../../frontend/src/assets/fallguys.png",
+    "Description": "Complete silly maps and puzzles with friends",
+    "Developer": "Mediatonic",
+    "CreationDate": "Tuesday Aug 4, 2020",
+    "Version": "1.x",
+    "Tags": ["Fall2020", "Adventure"],
+    "downloads": 1234567891011
 ]`
