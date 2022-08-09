5c11f3f1a97d603b96d112851fe040f7bcb16eb8
Elise Francis <elisefrancis@Elises-MacBook-Air.local>
2022-08-08 18:59:16 -0600

    added a user form for username and password and email

--- frontend/src/views/NewAccountView.vue
+++ frontend/src/views/NewAccountView.vue
@@ -1,13 +1,47 @@
 <template>
-	<div>
-		<!-- File uploads: FormData -->
-		<h1>This is where you create a new account.</h1>
-	</div>
+<h2>Create New User</h2>
+	<v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  	>
+    <v-text-field
+      v-model="username"
+      :counter="10"
+      label="Enter Username"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="userEmail"
+      label="Enter E-mail"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="userPassword"
+      :type="'password'"
+      name="input-10-1"
+      label="Enter Password"
+      hint="At least 8 characters"
+      counter
+    ></v-text-field>
+
+    <v-btn
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="validate"
+    >
+      Create User
+    </v-btn>
+  </v-form>
 </template>
 
 <script>
 export default {
 	name: 'NewAccount',
+  show1: false,
 
 }
 </script>
