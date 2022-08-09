545cf188e3915780eb17f8b1fef2bde8a8c9182b
Bryant Saunders - Laptop <bryantgsaunders@gmail.com>
2022-08-08 18:53:24 -0600

    Updated gcs_connection_test to have pass/fail conditions

--- backend/gcs/gcs_connection_test.go
+++ backend/gcs/gcs_connection_test.go
@@ -6,13 +6,13 @@ import (
 )
 
 func Test_downloadFile(t *testing.T) {
-	os.Setenv("STORAGE_EMULATOR_HOST", "http://localhost:4443")
-	err := downloadFile("breakout", "game/index.html")
+	os.Setenv("STORAGE_EMULATOR_HOST", "http://localhost:4443/storage/v1/b")
+	err := downloadFile("breakout", "index.html")
 	if err != nil {
-		print("bla")
+		t.Fatalf("Failed to download file.")
 	}
 
 	if err := os.Unsetenv("STORAGE_EMULATOR_HOST"); err != nil {
-		print("bla")
+		t.Fatalf("STORAGE_EMULATOR_HOST could not be unset.")
 	}
 }
