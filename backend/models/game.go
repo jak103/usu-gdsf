a9d0733c156d4686b575212431c291dbce64366d
pravinpoudel <prvnpoudel4@gmail.com>
2022-08-09 02:47:14 -0600

    game struct in models updated and validate binding removed

--- backend/models/game.go
+++ backend/models/game.go
@@ -3,17 +3,20 @@ package models
 import (
 	"time"
 )
+
 type Game struct {
-	Id           string `json:"Id"`
-	Name         string `json:"Name" validate:"required"`
-	Rating       float32 `json:"Rating" `
-	TimesPlayed  int `json:"TimesPlayed" validate:"required, number"`
-	ImagePath    string `json:"ImagePath" validate:"required, file"`
-	Description  string `json:"Description"`
-	Developer    string `json:"Developer" validate:"required, min=5, max=20"`
-	CreationDate time.Time `json:"CreationDate"`
-	Version      string `json:"Version"`
-	Tags         []string `json:"Tags"`
-	Downloads    int64 `json:"Downloads" validate:"number"` // firestore doesn't support uint64
-	DownloadLink string `json:"DownloadLink" validate:"required, uri"`
+	Id            string    `json:"Id"`
+	Name          string    `json:"Name"`
+	Rating        float32   `json:"Rating"`
+	TimesPlayed   int       `json:"TimesPlayed"`
+	ImagePath     string    `json:"ImagePath"`
+	Description   string    `json:"Description"`
+	Developer     string    `json:"Developer"`
+	CreationDate  time.Time `json:"CreationDate"`
+	Version       string    `json:"Version"`
+	Tags          []string  `json:"Tags"`
+	Downloads     int64     `json:"Downloads"` // firestore doesn't support uint64
+	DownloadLink  string    `json:"DownloadLink"`
+	reviewIds     []string  `json:"reviewIds"`
+	averageReview float64   `json:"averageReview"`
 }
