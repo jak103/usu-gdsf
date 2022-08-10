40213af11af3c1c499b25e720e2cb7427b74ce22
andrenguyen124 <andre.nguyen124@gmail.com>
2022-08-10 16:30:07 -0600

    Create a new test for homeView.spec.

--- frontend/test/components/homeView.spec.js
+++ frontend/test/components/homeView.spec.js
@@ -1,19 +1,22 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import HomeView from "../../src/views/HomeView.vue"
 
-describe('HomeView Rendering', () =>{
+describe('HomeView Rendering', () => {
     const testTitle = 'HomeView';
     const testAuthor = 'Game Author'
     const testDescription = 'Game Description'
-    const wrapper = shallowMount( HomeView, {
-        propsData : {
-            name : testTitle,
-            // author : testAuthor,
-            // description : testDescription,
-        },
-    });
 
-    test('Title', () => {
-        expect(wrapper.props().HomeView.testTitle).toBe(testTitle);
-    });
+    const wrapper = mount(HomeView);
+
+
+    test('renders a form with inputs', () => {
+        //check if the form is rendered
+        expect(wrapper.find('v-container').exists()).toBe(true)
+    }),
+
+    test('renders a form with inputs', () => {
+        //check if the form is rendered
+        expect(wrapper.find('v-row').exists()).toBe(true)
+      })
+
 })
