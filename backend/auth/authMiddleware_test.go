ba6614b48833a53bc0d6ce7aba2bcd3b349d211c
sriyapati <gorrepatisriya@gmail.com>
2022-08-10 16:32:36 -0600

    authMiddleware_test.go added admin functio

--- backend/auth/authMiddleware_test.go
+++ backend/auth/authMiddleware_test.go
@@ -19,27 +19,84 @@ func handlerStub(ctx echo.Context) error {
 }
 
 
-func TestMiddlewareWithexpiredToken(t *testing.T) {
-        e := echo.New()
-        rec := httptest.NewRecorder()
-        req := httptest.NewRequest(http.MethodGet, "/_testStub", nil)
-        c := e.NewContext(req, rec)
+// func TestMiddlewareWithexpiredToken(t *testing.T) {
+//         e := echo.New()
+//         rec := httptest.NewRecorder()
+//         req := httptest.NewRequest(http.MethodGet, "/_testStub", nil)
+//         c := e.NewContext(req, rec)
 
         
 
-        req.Header.Set("Cookie", ACCESS_TOKEN_COOKIE_KEY + "=eyJUeXBlIjowLCJFeHBpcmF0aW9uIjoxNjU5NDAyNTI5OTg1LCJVc2VySWQiOjQyLCJVc2VyVHlwZSI6MSwiVXNlckVtYWlsIjoidGVzdGluZ0BleGFtcGxlLmNvbSJ9fGE5N2QwMjViYzhkNzc2YjQwZjkyNmExNTBkMzg3OWEyY2Q5YTNmZTVkYTA1YTIyM2VlMDRmMDZlNGQxODFkZGI")
-        stubRequest := RequireAuthorization(handlerStub, false)
+//         req.Header.Set("Cookie", ACCESS_TOKEN_COOKIE_KEY + "=eyJUeXBlIjowLCJFeHBpcmF0aW9uIjoxNjU5NDAyNTI5OTg1LCJVc2VySWQiOjQyLCJVc2VyVHlwZSI6MSwiVXNlckVtYWlsIjoidGVzdGluZ0BleGFtcGxlLmNvbSJ9fGE5N2QwMjViYzhkNzc2YjQwZjkyNmExNTBkMzg3OWEyY2Q5YTNmZTVkYTA1YTIyM2VlMDRmMDZlNGQxODFkZGI")
+//         stubRequest := RequireAuthorization(handlerStub, false)
 
-        assert.Error(t, stubRequest(c))
-//         if assert.NOError(t, stubRequest(c)) {
+//         assert.Error(t, stubRequest(c))
+// //         if assert.NOError(t, stubRequest(c)) {
+// //                 respBody := rec.Body.String()
+// //                 assert.Equal(t, respBody, HANDLER_STUB_SUCCESS)
+// //         }
+//  }
+
+
+// // Provide access token cookie that is valid and not expired
+// func TestMiddlewareWithValidToken(t *testing.T) {
+//         e := echo.New()
+//         rec := httptest.NewRecorder()
+//         req := httptest.NewRequest(http.MethodGet, "/_testStub", nil)
+//         c := e.NewContext(req, rec)
+
+//         params := TokenParams{
+//                 Type: ACCESS_TOKEN,
+//                 UserId: 39,
+//                 UserType: REGULAR_USER,
+//                 UserEmail: "testing@example.com",
+//         }
+
+//         req.Header.Set("Cookie", ACCESS_TOKEN_COOKIE_KEY + "=" + GenerateToken(params))
+//         stubRequest := RequireAuthorization(handlerStub, false)
+
+//         if assert.NoError(t, stubRequest(c)) {
 //                 respBody := rec.Body.String()
 //                 assert.Equal(t, respBody, HANDLER_STUB_SUCCESS)
 //         }
- }
+// }
+
+// //Test to not provide an access token
+
+// func TestMiddlewareNoToken(t *testing.T) {
+//         e := echo.New()
+//         rec := httptest.NewRecorder()
+//         req := httptest.NewRequest(http.MethodGet, "/_testStub", nil)
+//         c := e.NewContext(req, rec)
+
+//         stubRequest := RequireAuthorization(handlerStub, false)
+//         assert.Error(t, stubRequest(c))
+// }
 
 
-// Provide access token cookie that is valid and not expired
-func TestMiddlewareWithValidToken(t *testing.T) {
+// //Provide access token cookie that is INVALID
+
+// func TestMiddlewareWitheinvalidToken(t *testing.T) {
+//         e := echo.New()
+//         rec := httptest.NewRecorder()
+//         req := httptest.NewRequest(http.MethodGet, "/_testStub", nil)
+//         c := e.NewContext(req, rec)
+
+        
+
+//         req.Header.Set("Cookie", ACCESS_TOKEN_COOKIE_KEY + "=hello world")
+//         stubRequest := RequireAuthorization(handlerStub, false)
+
+//         assert.Error(t, stubRequest(c))
+// //         if assert.NOError(t, stubRequest(c)) {
+// //                 respBody := rec.Body.String()
+// //                 assert.Equal(t, respBody, HANDLER_STUB_SUCCESS)
+// //         }
+//  }
+
+ // Provide a token for a REGULAR_USER and pass `true` in for `requireAdmin `
+
+ func TestMiddlewareAdminToken(t *testing.T) {
         e := echo.New()
         rec := httptest.NewRecorder()
         req := httptest.NewRequest(http.MethodGet, "/_testStub", nil)
@@ -53,43 +110,8 @@ func TestMiddlewareWithValidToken(t *testing.T) {
         }
 
         req.Header.Set("Cookie", ACCESS_TOKEN_COOKIE_KEY + "=" + GenerateToken(params))
-        stubRequest := RequireAuthorization(handlerStub, false)
-
-        if assert.NoError(t, stubRequest(c)) {
-                respBody := rec.Body.String()
-                assert.Equal(t, respBody, HANDLER_STUB_SUCCESS)
-        }
-}
-
-//Test to not provide an access token
-
-func TestMiddlewareNoToken(t *testing.T) {
-        e := echo.New()
-        rec := httptest.NewRecorder()
-        req := httptest.NewRequest(http.MethodGet, "/_testStub", nil)
-        c := e.NewContext(req, rec)
-
-        stubRequest := RequireAuthorization(handlerStub, false)
-        assert.Error(t, stubRequest(c))
-}
-
-
-//Provide access token cookie that is INVALID
-
-func TestMiddlewareWitheinvalidToken(t *testing.T) {
-        e := echo.New()
-        rec := httptest.NewRecorder()
-        req := httptest.NewRequest(http.MethodGet, "/_testStub", nil)
-        c := e.NewContext(req, rec)
-
-        
-
-        req.Header.Set("Cookie", ACCESS_TOKEN_COOKIE_KEY + "=hello world")
-        stubRequest := RequireAuthorization(handlerStub, false)
+        stubRequest := RequireAuthorization(handlerStub, true)
 
         assert.Error(t, stubRequest(c))
-//         if assert.NOError(t, stubRequest(c)) {
-//                 respBody := rec.Body.String()
-//                 assert.Equal(t, respBody, HANDLER_STUB_SUCCESS)
-//         }
- }
+
+}
