4343cbfe6f58503a0d365740bb2212ed2d86c13c
Bridget Staley <lynnbridgegirl@gmail.com>
2022-08-09 21:25:39 -0600

    Added axios to application with example

--- frontend/src/components/GameCarousel.vue
+++ frontend/src/components/GameCarousel.vue
@@ -1,12 +1,17 @@
 <template>
   <v-card width="100%">
     <v-carousel cycle :interval="cycleTime">
-      <v-carousel-item v-for="color in colors" :key="color">
-        <v-sheet :color="color" height="100%" tile>
+      <v-carousel-item v-for="game in gameData.slice(0,5)" :key="game">
+        <v-sheet height="100%" tile>
           <v-row class="fill-height" align="center" justify="center">
-            <div class="text-h2">
-              {{ color }}
-            </div>
+            <v-col align="center" justify="center">
+              <v-row class="text-h2 fill-height" align="center" justify="center">
+                Title: {{ game.title}}
+              </v-row>
+              <v-row align="center" justify="center">
+                Description: {{ game.description }}
+              </v-row>
+            </v-col>
           </v-row>
         </v-sheet>
       </v-carousel-item>
@@ -15,12 +20,23 @@
 </template>
 
 <script>
+
 export default {
   name: 'GameCarousel',
 
   data: () => ({
     colors: ['primary','secondary','info','warning','white','teal', 'watermelon'],
     cycleTime: 4000,
+    gameData: []
   }),
+
+  async mounted() {
+    try{
+      const response = await this.$axios.get('/game');
+      this.gameData = response.data[0];
+    } catch {
+      console.log('Error')
+    }
+  }
 }
 </script>
